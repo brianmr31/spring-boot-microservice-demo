4ac9f7a5c6af68fc4960d6fc23785e5f0bf0ea33
Brian.Foxi <brian.raharjo@foxilive.com>
2024-04-29 22:34:57 +0700

    add handler exception

---
+++ logging/src/main/java/test/danamon/microservice/logging/config/BadRequestException.java
@@ -0,0 +1,8 @@
+package test.danamon.microservice.logging.config;
+
+public class BadRequestException extends Exception {
+    
+    public BadRequestException(String message){
+        super(message);
+    }
+}
